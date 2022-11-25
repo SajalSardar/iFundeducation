7b308d573d608f2273a04fa8b26bedc34d77a4ab
sajalsardar <sajal2.stars@gmail.com>
2022-11-26 00:33:23 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 $(function($){
 // menu js 
-var main_menu = $('#main_navigation').offset().top;
+  var main_menu = $('#main_navigation').offset().top;
 
-$(window).on('scroll',function(){
+  $(window).on('scroll',function(){
 
-  var scroll_value = $(window).scrollTop();
+    var scroll_value = $(window).scrollTop();
 
-  if(scroll_value > main_menu){
-    $('#main_navigation').addClass('menuFix');
-  }else{
-    $('#main_navigation').removeClass('menuFix');
-  }
+    if(scroll_value > main_menu){
+      $('#main_navigation').addClass('menuFix');
+    }else{
+      $('#main_navigation').removeClass('menuFix');
+    }
 
-});
+  });
+
+  $('.mobile_menu_icon').on('click', function(){
+    $('.account_menu').addClass('fixed');
+  });
+
+  $('.account_menu .close_icon').on('click', function(){
+    $('.account_menu').removeClass('fixed');
+  });
 
 });
